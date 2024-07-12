03dd9827b31deee72ae7e2bdc88349c79d66047b
NoothangiBharathi <noothangi.bharathi1999@gmail.com>
2024-07-12 13:54:12 +0530

    Adding Overview and App inner pages

--- script.js
+++ script.js
@@ -2,28 +2,28 @@
 const testimonials = [
   {
     name: "Eva Sawyer",
-    job: "CEO, Fashworks",
+    job: "Mountener",
     image: "../images/testimage1.jpeg",
     testimonial:
       "Neque volutpat ac tincidunt vitae semper quis lectus nulla at volutpat diam ut venenatis tellus in metus vulputate eu scelerisque felis imperdiet proin fermentum leo vel orci porta non pulvinar neque laoreet suspendisse interdum consectetur",
   },
   {
     name: "Katey Topaz",
-    job: "Developer, TechCrew",
+    job: "Delivery, TechCrew",
     image: "../images/testimage2.jpeg",
     testimonial:
       "Elementum tempus egestas sed sed risus pretium quam vulputate dignissim suspendisse in est ante in nibh mauris cursus mattis molestie a iaculis at erat pellentesque adipiscing commodo elit at imperdiet dui accumsan sit amet nulla",
   },
   {
     name: "Jae Robin",
-    job: "UI Designer, Affinity Agency",
+    job: "Product designer, Affinity Agency",
     image: "../images/testimage3.jpeg",
     testimonial:
       "Orci nulla pellentesque dignissim enim sit amet venenatis urna cursus eget nunc scelerisque viverra mauris in aliquam sem fringilla ut morbi tincidunt augue interdum velit euismod in pellentesque massa placerat duis ultricies lacus sed turpis",
   },
   {
     name: "Nicola Blakely",
-    job: "CEO,Zeal Wheels",
+    job: "CEO, CEO of Affinity Agency",
     image: "../images/testimage4.jpeg",
     testimonial:
       "Sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit",
